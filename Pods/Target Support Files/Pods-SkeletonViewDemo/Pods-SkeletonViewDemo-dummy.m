204b91a5d5447de36716ac7f9482cdb5eeab347a
MeetThanki <meetthanki39@gmail.com>
2020-07-09 15:55:47 +0530

    Initial commit

---
+++ Pods/Target Support Files/Pods-SkeletonViewDemo/Pods-SkeletonViewDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SkeletonViewDemo : NSObject
+@end
+@implementation PodsDummy_Pods_SkeletonViewDemo
+@end
---
+++ SkeletonViewDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SkeletonViewDemo
+//
+//  Created by Meet Thanki on 09/07/20.
+//  Copyright © 2020 Meet Thanki. All rights reserved.
+//
+
+import UIKit
+import SkeletonView
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.showAnimatedSkeleton()
+        
+    }
+
+
+}
+
